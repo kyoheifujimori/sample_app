2b976569b624efdfbb8e558d9c99e9936e68d252
kyoheifujimori <kyohei.fujimori@g.dreamcareer.co.jp>
2024-05-24 10:34:43 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './my_picture.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <h1 id="introduction">Introduction</h1>
+        <div>
+          <img src={logo} alt="logo" />
+          <div>
+            <p>出身地：長野県</p>
+            <p>好きなこと：バイク、お酒、料理</p>
+            <p>好きな言語：python</p>
+            <p>ひと言：大学一年生から言語を触り始めた新卒社会人です。
+              プログラミングを深く学びたいためSE（システムエンジニア）職
+              を選びました。これからもりもり成長していく予定なのでよろしく
+              おねがいします！！！
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
